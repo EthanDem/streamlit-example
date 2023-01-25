3bc6645fabe5e733103e78a6e74df456cb1e01da
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-01-25 09:18:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,24 @@ import pandas as pd
 st.set_page_config(page_title="My Webpage", page_icon='tada', layout='wide')
 
 st.header("Pandas Website Test For CSV Files")
-st.subheader("You will receive the red error until you input the exact file path to which you would like to use.")
-st.write("It must be the exact file path as well as a csv, for example /Users/You/Dekstop/yourfile.csv")
-st.write("For mac, visit here on how to get an exact file path : https://support.apple.com/guide/mac-help/get-file-folder-and-disk-information-on-mac-mchlp1774/mac")
+st.subheader("Upload a file that you would like to get information from.")
+
+
+
+
 
 def read_csv(file_path):
     df = pd.read_csv(file_path)
     return df
 
+
+
+
+fileupload = st.file_uploader(label=" ")
+
 def answer_question(df, column_name, question):
     if column_name not in df.columns:
-        return "Sorry, that column does not exist in the data."
+        return "Sorry, that column does not exist in the data. Please check punctuation and ensure that you pressed enter."
     else:
         if question == "average":
             return df[column_name].mean()
@@ -27,15 +34,20 @@ def answer_question(df, column_name, question):
         else:
             return "Sorry, I don't understand your question."
 
-file_path = st.text_input('Input EXACT file path here, and press enter.')
+
+
+#file_path = st.text_input('Input EXACT file path here, and press enter.')
 #file_path = "C:/Users/ethan/Cookies/Desktop/streamlit1.csv.csv"
-df = read_csv(file_path)
+
+
 
 @st.cache(allow_output_mutation=True)
 def get_data():
     return []
-
-question = st.text_input("What Function? (Type average, minimum, maximum, or standard deviation)").lower()
-column_name = st.text_input("Column Name? (Use correct punctuation)")
-answer = answer_question(df, column_name, question)
-st.write(answer)
+if fileupload:
+    df = read_csv(fileupload)
+    question = st.text_input("What Function? (Type average, minimum, maximum, or standard deviation)").lower()
+    column_name = st.text_input("Column Name? (Use correct punctuation)")
+    answer = answer_question(df, column_name, question)
+    st.write(answer)
+#st.write(answer)
