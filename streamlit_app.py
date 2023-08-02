fe7607ee29bbb90f9088282e556479e92e84f685
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-02 12:05:40 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,53 +1,17 @@
 import streamlit as st
-import pandas as pd
+import requests
 
-st.set_page_config(page_title="My Webpage", page_icon='tada', layout='wide')
+# Get parameters from the user
+param1 = st.text_input('Enter the first parameter:')
 
-st.header("Pandas Data Sorter Test")
-st.subheader("Upload a file that you would like to get information from.")
+if st.button('Scrape'):
+    # Send a request to the Flask app
+    response = requests.post('http://localhost:5000/scrape', json={'param1': param1, 'param2': param2})
 
-
-
-
-
-def read_csv(file_path):
-    df = pd.read_csv(file_path)
-    return df
-
-
-
-
-fileupload = st.file_uploader(label=" ")
-
-def answer_question(df, column_name, question):
-    if column_name not in df.columns:
-        return "Sorry, that column does not exist in the data. Please check punctuation and ensure that you pressed enter."
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Display the scraped data
+        st.write(response.json()['data'])
     else:
-        if question == "average":
-            return df[column_name].mean()
-        elif question == "minimum":
-            return df[column_name].min()
-        elif question == "maximum":
-            return df[column_name].max()
-        elif question == "standard deviation":
-            return df[column_name].std()
-        else:
-            return "Sorry, I don't understand your question."
-
-
-
-#file_path = st.text_input('Input EXACT file path here, and press enter.')
-#file_path = "C:/Users/ethan/Cookies/Desktop/streamlit1.csv.csv"
-
-
-
-@st.cache(allow_output_mutation=True)
-def get_data():
-    return []
-if fileupload:
-    df = read_csv(fileupload)
-    question = st.text_input("What Function? (Type average, minimum, maximum, or standard deviation)").lower()
-    column_name = st.text_input("Column Name? (Use correct punctuation)")
-    answer = answer_question(df, column_name, question)
-    st.write(answer)
-#st.write(answer)
+        # Display the error message
+        st.error(response.json()['message'])
