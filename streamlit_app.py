7e194794f03ffe55ebbc2725170beafb70f69122
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-07 13:11:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,14 @@ openai.api_key = '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'
 messages = []
 
 system_msg = st.text_input("What type of chatbot would you like to create?\n")
-messages.append({"role": "system", "content":system_msg})
+
+#messages.append({"role": "system", "content":system_msg})
 
 x = st.text_input("Input question here")
 button_pressed = st.button("Send question")
 
 if button_pressed:
+    messages.append({"role": "system", "content":system_msg})
     message = input()
     messages.append({"role": "user", "content": message})
     response = openai.ChatCompletion.create(
