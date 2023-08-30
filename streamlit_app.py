513012286b8bf1092f634b4102d5940b07323553
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 15:28:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ url_to_scrape = st.text_input("Enter the URL you want to scrape")
 email_to_share = st.text_input("Enter your email to share the Google Sheet")
 
 if st.button("Start Scraping"):
-    response = requests.post("http://localhost:5000/scrape", json={"url": url_to_scrape, "email": email_to_share})
+    response = requests.post("https://link-grabber-backend.icecube1513.repl.co", json={"url": url_to_scrape, "email": email_to_share})
     st.write("Links are being extracted, this may take a while. Once they are completed, they will be shared with you via Google Sheets.")
