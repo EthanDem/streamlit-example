0330c6d85e421aff1d842add1de2a4d248c05457
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 13:21:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,17 @@
 import streamlit as st
 import requests
 
-# Your Flask API URL
 api_url = "https://link-grabber-backend.icecube1513.repl.co/scrape"
 
-# Streamlit UI
 url_to_scrape = st.text_input("Enter the URL you want to scrape:")
 if st.button("Scrape"):
     st.write("Scraping in progress. This may take a while.")
     if url_to_scrape:
         response = requests.post(api_url, json={"url": url_to_scrape})
         if response.status_code == 200:
-            st.write("Scraping completed!")
+            st.success("Scraping completed!")
             scraped_data = response.json()  # Adjusted this line
 
-            # Display the scraped data
             for link in scraped_data:
                 st.write(link)
         else:
