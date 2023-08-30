e8e1aed4df29c714b4c6523caa37360d37e322e6
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 12:54:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import requests
-import pandas as pd
 
-replit_url = "https://selenium-backend.icecube1513.repl.co/scrape"
+# Your Flask API URL
+api_url = "link-grabber-backend.icecube1513.repl.co/scrape"
 
+# Streamlit UI
 url_to_scrape = st.text_input("Enter the URL you want to scrape:")
 if st.button("Scrape"):
     if url_to_scrape:
-        response = requests.post(replit_url, json={"url": url_to_scrape})
+        response = requests.post(api_url, json={"url": url_to_scrape})
         if response.status_code == 200:
             st.write("Scraping completed!")
             scraped_data = response.json()['data']
 
-            df = pd.DataFrame(scraped_data, columns=['Scraped Links'])
-
-            st.write(df, index=False)
+            # Display the scraped data
+            for link in scraped_data:
+                st.write(link)
         else:
             st.write(f"An error occurred: {response.content}")
     else:
