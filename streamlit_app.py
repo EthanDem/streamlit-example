dcc562df68f802d5130a7a16d0b29e66eca77cc4
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 15:26:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,12 @@
 import streamlit as st
 import requests
 
-api_url = "https://link-grabber-backend.icecube1513.repl.co/scrape"
-st.header("URL Grabber")
-url_to_scrape = st.text_input("Enter the URL you want to fetch all webpages from")
+st.title("Web Scraper")
 
-if st.button("Scrape"):
-    st.write("Scraping in progress. This may take a while.")
-    if url_to_scrape:
-        response = requests.post(api_url, json={"url": url_to_scrape})
-        if response.status_code == 200:
-            st.success("Scraping completed!")
-            scraped_data = response.json()  # Adjusted this line
+url_to_scrape = st.text_input("Enter the URL you want to scrape")
 
-            for link in scraped_data:
-                st.write(link)
-        else:
-            st.write(f"An error occurred: {response.content}")
-    else:
-        st.write("Please enter a URL.")
+email_to_share = st.text_input("Enter your email to share the Google Sheet")
+
+if st.button("Start Scraping"):
+    response = requests.post("http://localhost:5000/scrape", json={"url": url_to_scrape, "email": email_to_share})
+    st.write("Links are being extracted, this may take a while. Once they are completed, they will be shared with you via Google Sheets.")
