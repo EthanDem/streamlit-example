90f622b91a53222036dd6e07fe145f5daabaaa7f
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 15:34:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,25 @@
 import streamlit as st
 import requests
 
-st.title("Web Scraper")
+# Your Flask API URL
+api_url = "https://link-grabber-backend.icecube1513.repl.co/scrape"
 
-url_to_scrape = st.text_input("Enter the URL you want to scrape")
+# Streamlit UI
+url_to_scrape = st.text_input("Enter the URL you want to scrape:")
 
 email_to_share = st.text_input("Enter your email to share the Google Sheet")
 
-if st.button("Start Scraping"):
-    response = requests.post("https://link-grabber-backend.icecube1513.repl.co", json={"url": url_to_scrape, "email": email_to_share})
-    st.write("Links are being extracted, this may take a while. Once they are completed, they will be shared with you via Google Sheets.")
+if st.button("Scrape"):
+    if url_to_scrape:
+        response = requests.post(api_url, json={"url": url_to_scrape, "email": email_to_share})
+        if response.status_code == 200:
+            st.write("Scraping completed!")
+            scraped_data = response.json()  # Adjusted this line
+
+            # Display the scraped data
+            for link in scraped_data:
+                st.write(link)
+        else:
+            st.write(f"An error occurred: {response.content}")
+    else:
+        st.write("Please enter a URL.")
