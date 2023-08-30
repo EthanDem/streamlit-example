a5a41d7dc1d6891efb7314a4aa37319306c3b100
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-08-30 12:39:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 
 replit_url = "https://selenium-backend.icecube1513.repl.co/scrape"
-
 
 url_to_scrape = st.text_input("Enter the URL you want to scrape:")
 if st.button("Scrape"):
@@ -11,7 +11,11 @@ if st.button("Scrape"):
         response = requests.post(replit_url, json={"url": url_to_scrape})
         if response.status_code == 200:
             st.write("Scraping completed!")
-            st.write(response.json())
+            scraped_data = response.json()['data']
+
+            df = pd.DataFrame(scraped_data, columns=['Scraped Links'])
+         
+            st.table(df)
         else:
             st.write(f"An error occurred: {response.content}")
     else:
